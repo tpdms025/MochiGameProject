31475aa1c133e620d2869a575edae4e1f37aa2b3
tpdms025 <seyang.dev@gmail.com>
2022-01-05 13:50:55 +0900

    Add Unity Project.

---
+++ ClickerGameProject/Assets/Scripts/GlodController.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlodController : MonoBehaviour
+{
+    #region Data
+
+    static readonly string[] currencyUnits = new string[]
+    {
+        "",""
+    };
+
+    #endregion
+
+    #region Fields
+    #endregion
+
+    #region Unity methods
+
+
+    #endregion
+
+    #region Methods
+
+
+    #endregion
+
+    #region Private Methods
+
+    #endregion
+
+}
