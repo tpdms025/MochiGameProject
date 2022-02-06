8fc6906f436975eee09561b074448d407b781189
tpdms025 <seyang.dev@gmail.com>
2022-02-06 23:42:59 +0900

    Design the overall structure of the pop-up window and object pool.

---
+++ ClickerGameProject/Assets/02.Scripts/Utility/ObjectPool/DemoObject.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DemoObject : MonoBehaviour
+{
+    public string poolItemName = "DamagePopupObj";
+
+    private DemoObject damagePopup;
+    
+    Transform _parent;
+
+    private void Awake()
+    {
+    }
+
+    private void Initialize()
+    {
+    }
+
+    public DemoObject Create(Vector3 position, int damageAmount, bool isCriticalHit,bool isMiss, Transform parent = null)
+    {
+        _parent = parent;
+        GameObject damageObject = ObjectPool.Instance.PopFromPool(poolItemName, parent);
+        damagePopup = damageObject.transform.GetComponent<DemoObject>();
+        damagePopup.Initialize();
+        damagePopup.transform.position = position;
+        damageObject.SetActive(true);
+
+        return damagePopup;
+    }
+
+  
+
+
+}
