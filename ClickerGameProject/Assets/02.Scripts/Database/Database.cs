4cb6bb95417b76d6c818e7e9c5891d4701301630
tpdms025 <seyang.dev@gmail.com>
2022-04-15 02:30:13 +0900

    Add PlayerData

--- ClickerGameProject/Assets/02.Scripts/Database/Database.cs
+++ ClickerGameProject/Assets/02.Scripts/Database/Database.cs
@@ -5,21 +5,98 @@ using System;
 using System.Numerics;
 
 [Serializable]
-public class Database 
+public class Database
 {
-   
+
+    public class ValueOfLevel
+    {
+        //public int level;
+        public BigInteger amountPerTouch;
+        public BigInteger cost;
+    }
+
+    public class ProductOriginData
+    {
+        public string id;
+        public string type;
+        public string name;
+        public string spriteName;
+        public List<ValueOfLevel> levelTable;
+
+        public ProductOriginData(){ }
+        public ProductOriginData(string _name, string _spriteName, List<ValueOfLevel> _levelTable)
+        {
+            this.name = _name;
+            this.spriteName = _spriteName;
+            this.levelTable = _levelTable;
+        }
+    }
+
+    [Serializable]
+    public class Inventory
+    {
+    }
+
+    [Serializable]
+    public class PlayerData
+    {
+        //광석 정보
+        //마지막으로 소유한 Cell의 인덱스 번호
+        public int idx_lastOwned;
+
+        //마지막으로 소유한 Cell의 레벨
+        public int level_lastOwned;
+
+        //일꾼 정보
+        //소유한 셀 레벨에 대한 배열
+        public int[] workmanLevelList;
+
+
+        //누적 터치 횟수
+        public int touchCount;
+
+        //자동채굴 여부
+        public bool isAutoMining;
+
+        public PlayerData(int idx_lastUnlocked, int level_lastUnlocked, string workmanLevelList)
+        {
+            this.idx_lastOwned = idx_lastUnlocked;
+            this.level_lastOwned = level_lastUnlocked;
+            this.workmanLevelList = Array.ConvertAll(workmanLevelList.Split(','), int.Parse);
+        }
+    }
+
+
     public class BuffTable
     {
         public int id;
         public string name;
         public int time;
-        public int curTime;
-        public BuffTable(int id, string name, int time, int curTime)
+        public int remainingTime;
+        public BuffTable(int id, string name, int time, int remainingTime)
         {
             this.id = id;
             this.name = name;
             this.time = time;
-            this.curTime = curTime;
+            this.remainingTime = remainingTime;
+        }
+    }
+    public class SkillTable
+    {
+        public int id;
+        public string name;
+        public int buffTime;
+        public int buffRemainingTime;
+        public int maxCooldown;
+        public int cooldownRemaining;
+        public SkillTable(int id, string name, int buffTime, int buffRemainingTime, int maxCooldown, int cooldownRemaining)
+        {
+            this.id = id;
+            this.name = name;
+            this.buffTime = buffTime;
+            this.buffRemainingTime = buffRemainingTime;
+            this.maxCooldown = maxCooldown;
+            this.cooldownRemaining = cooldownRemaining;
         }
     }
 
--- ClickerGameProject/Assets/02.Scripts/Manager/DBManager.cs
+++ ClickerGameProject/Assets/02.Scripts/Manager/DBManager.cs
@@ -5,14 +5,12 @@ using UnityEngine;
 
 public class DBManager : MonoBehaviour
 {
-    #region Data
 
-    #endregion
 
     #region Fields
 
     private static DBManager instance = null;
-    public static DBManager Instance
+    public static DBManager Inst
     {
         get { return instance; }
         set { instance = value; }
@@ -34,29 +32,214 @@ public class DBManager : MonoBehaviour
         {
             Destroy(this.gameObject);
         }
+    }
 
-        //MoneyManager.Instance.onJewelChanged += SaveJewel;
+    private void Start()
+    {
     }
 
     #endregion
 
-    #region Methods
-
-    private void LoadDBCurrency()
+    public void LoadData()
     {
-
+        LoadPlayerData();
+        LoadOreData();
+        LoadWorkmanData();
+        LoadBuffTable();
+        LoadSkillTable();
     }
 
-    private void SaveDBCurrency()
-    {
+    #region Buff
 
+    //버프 엑셀 데이터
+    private List<Database.BuffTable> m_buffTables;
+
+    public List<Database.BuffTable> LoadBuffTable()
+    {
+        List<Database.BuffTable> list = new List<Database.BuffTable>();
+        list.Add(new Database.BuffTable(0, "터치증가", 10, 7));
+        list.Add(new Database.BuffTable(1, "일꾼 생산량 증가", 1800, 1800));
+        list.Add(new Database.BuffTable(2, "피버 모드 ", 5, 5));
+
+        m_buffTables = list;
+        return m_buffTables;
+    }
+
+    public Database.BuffTable GetBuffTable(int idx)
+    {
+        return m_buffTables[idx];
     }
 
     #endregion
 
-    #region Jewel_Methods
-    private void SaveJewel(BigInteger _jewel)
+    #region Skill
+
+    //스킬 엑셀 데이터
+    private List<Database.SkillTable> m_skillTables;
+
+    public List<Database.SkillTable> LoadSkillTable()
     {
+        List<Database.SkillTable> list = new List<Database.SkillTable>();
+        list.Add(new Database.SkillTable(0, "자동 채굴 ", 60, 60,1800,1800));
+
+        m_skillTables = list;
+        return m_skillTables;
     }
+
+    public Database.SkillTable GetSkillTable(int idx)
+    {
+        return m_skillTables[idx];
+    }
+
+    #endregion
+
+
+    #region Ore Excel Data
+
+    // 광석의 엑셀 데이터
+    private List<Database.ProductOriginData> m_oreOriginDatas;
+
+    private void LoadOreData()
+    {
+        m_oreOriginDatas = GetTempOreInfo();
+    }
+
+    public List<Database.ProductOriginData> GetOreOriginDatas()
+    {
+        if (m_oreOriginDatas == null) return null;
+        return m_oreOriginDatas;
+    }
+
+    private string []oreSpriteNames = { "Ore_Ruby" , "Ore_PinkSapphire", "Ore_LightColorado",
+    "Ore_Citrine","Ore_Peridot","Ore_Emerald","Ore_BlueZircon","Ore_Cobalt","Ore_Ametrine",
+    "Ore_Iolite","Ore_Diamond"};
+    private List<Database.ProductOriginData> GetTempOreInfo()
+    {
+        List<Database.ProductOriginData> table = new List<Database.ProductOriginData>();
+        int ascii = (int)'A';
+        for (int i = 0; i < 11; i++)
+        {
+            Database.ProductOriginData data = new Database.ProductOriginData();
+            data.name = "광석" + System.Convert.ToString(System.Convert.ToChar(ascii + i));
+            data.spriteName = oreSpriteNames[i];
+            data.levelTable = GetTempOreLevelValue(i);
+
+            table.Add(data);
+        }
+        return table;
+    }
+
+    /// <summary>
+    /// 원하는 인덱스의 광석 레벨정보들을 가져온다.
+    /// </summary>
+    /// <returns></returns>
+    private List<Database.ValueOfLevel> GetTempOreLevelValue(int num)
+    {
+        List<Database.ValueOfLevel> table = new List<Database.ValueOfLevel>();
+        for (int i = 0; i < 5; i++)
+        {
+            Database.ValueOfLevel data = new Database.ValueOfLevel();
+            //data.level = i;
+            if (i == 0)
+                data.amountPerTouch = 0;
+            else
+                data.amountPerTouch = BigInteger.Pow(2 + num, i);
+            data.cost = BigInteger.Pow(3 + num, i);
+            table.Add(data);
+        }
+        return table;
+    }
+
+
+
+    #endregion
+
+    #region Workman Excel Data
+
+    // 광석의 엑셀 데이터
+    private List<Database.ProductOriginData> m_workmanOriginDatas;
+
+    private void LoadWorkmanData()
+    {
+        m_workmanOriginDatas = GetTempWorkmanInfo();
+    }
+
+    public List<Database.ProductOriginData> GetWorkmanOriginDatas()
+    {
+        if (m_workmanOriginDatas == null) return null;
+        return m_workmanOriginDatas;
+    }
+
+    private string[] workmanSpriteNames = { "Work_Cat" , "Work_Rabbit", "Work_Hedgehog",
+    "Work_Mole","Work_Dog","Work_Fox","Work_Elk","Work_Dragon"};
+    private List<Database.ProductOriginData> GetTempWorkmanInfo()
+    {
+        List<Database.ProductOriginData> table = new List<Database.ProductOriginData>();
+        int ascii = (int)'A';
+        for (int i = 0; i < 8; i++)
+        {
+            Database.ProductOriginData data = new Database.ProductOriginData();
+            data.name = "모찌" + System.Convert.ToString(System.Convert.ToChar(ascii + i));
+            data.spriteName = workmanSpriteNames[i];
+            data.levelTable = GetTempWorkmanLevelValue(i);
+
+            table.Add(data);
+        }
+        return table;
+    }
+
+    /// <summary>
+    /// 원하는 인덱스의 광석 레벨정보들을 가져온다.
+    /// </summary>
+    /// <returns></returns>
+    private List<Database.ValueOfLevel> GetTempWorkmanLevelValue(int num)
+    {
+        List<Database.ValueOfLevel> table = new List<Database.ValueOfLevel>();
+        for (int i = 0; i < 100; i++)
+        {
+            Database.ValueOfLevel data = new Database.ValueOfLevel();
+            //data.level = i;
+            if (i == 0)
+                data.amountPerTouch = 0;
+            else
+                data.amountPerTouch = BigInteger.Pow(2 + num, i);
+            data.cost = BigInteger.Pow(3 + num, i);
+            table.Add(data);
+        }
+        return table;
+    }
+
+
+
+    #endregion
+
+    #region PlayerData
+
+    // 플레이어 데이터
+    private Database.PlayerData m_playerData;
+
+    public Database.PlayerData PlayerData
+    {
+        get { return m_playerData; }
+        private set { m_playerData = value; }
+    }
+
+    public void LoadPlayerData()
+    {
+        //임시
+        string workmanLevelString = "0,0,0,0,0,0,0,0";
+        m_playerData = new Database.PlayerData(0, 1, workmanLevelString);
+        m_playerData.touchCount = 0;
+        m_playerData.isAutoMining = false;
+    }
+
+    /// <summary>
+    /// 마지막으로 소유한 광석데이터를 가져온다.
+    /// </summary>
+    public Database.ProductOriginData GetLastOreDataOwned()
+    {
+        return m_oreOriginDatas[m_playerData.idx_lastOwned];
+    }
+
     #endregion
 }
