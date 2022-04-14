f7a082147c1d43dfd203b03e3a1ec25cac4f2b06
tpdms025 <seyang.dev@gmail.com>
2022-04-15 02:34:42 +0900

    add spawn timer

--- ClickerGameProject/Assets/02.Scripts/Character/BonusBat.cs
+++ ClickerGameProject/Assets/02.Scripts/Character/BonusBat.cs
@@ -7,10 +7,10 @@ public class BonusBat : MonoBehaviour
 {
     [Header("Attribute")]
     //도착 위치
-    [SerializeField] private Transform targetPos;
+    private Vector3 targetPoint;
 
     //시작 위치
-    private Vector3 startPos;
+    private Vector3 startPoint;
 
     //이동 속도
     [SerializeField] private float speed = 2.0f;
@@ -23,17 +23,33 @@ public class BonusBat : MonoBehaviour
 
     [Space(10)]
     [Header("Spawn")]
+    //시간 감소 비율
+    public float reductionRate;
+
     //기본 스폰 시간
     private float baseSpawnTime;
 
-    //시간 감소 퍼센트
-    public float reductionPercentage;
-
     //최종 스폰 시간
-    public float secondsBetweenSpawn;
+    public float finalSpawnTime;
 
-    //경과 시간
-    [SerializeField]private float elapsedTime;
+    //스폰 지속 시간
+    [SerializeField]private float spawnDuration;
+
+
+    [Space(10)]
+    [Header("Flight")]
+    //총 이동 시간
+    private const float flightTime = 8.0f;
+
+    //총 이동한 거리
+    public float totaldistance = 0.0f;
+
+    //매프레임마다 이동할 단위
+    private float distUnit = 0.0f;
+
+
+
+    private Animator animator;
 
 
 
@@ -41,26 +57,31 @@ public class BonusBat : MonoBehaviour
     #region Unity methods
     private void Awake()
     {
-        startPos = transform.position;
-        baseSpawnTime = 150.0f;
+        animator = GetComponent<Animator>();
 
+        startPoint = transform.position + new Vector3(0.0f, Camera.main.orthographicSize * 0.2f);
+        targetPoint = new Vector3(5.0f, 3.72f, 0);
+        distUnit = (targetPoint - startPoint).x / flightTime;
+
+        baseSpawnTime = 150.0f;
         //강화할때 변경될 예정
         //임시값
-        reductionPercentage = 0.0f;
-        reductionPercentage = 90.0f;
+        reductionRate = 0.0f;
+        reductionRate = 90.0f;
         ChangeSpawnTime();
     }
 
     private void Start()
     {
+        transform.position = startPoint;
         OnReset();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        //특정 콜라이더를 충돌하면 리셋한다.
         if (isFlying && collision.gameObject.CompareTag("NotFlyingZone"))
         {
-            //리셋하기
             OnReset();
         }
     }
@@ -71,22 +92,25 @@ public class BonusBat : MonoBehaviour
 
     public void ChangeSpawnTime()
     {
-        secondsBetweenSpawn = baseSpawnTime - (baseSpawnTime * reductionPercentage * 0.01f);
+        finalSpawnTime = baseSpawnTime - (baseSpawnTime * reductionRate * 0.01f);
     }
 
 
-    public void OnReset()
+    /// <summary>
+    /// 위치와 스폰시간을 리셋한다.
+    /// </summary>
+    private void OnReset()
     {
         EndTheFlight();
-        StartCoroutine(Cor_Timer());
+        StartCoroutine(Cor_SpawnTimer());
     }
 
-    private IEnumerator Cor_Timer()
+    private IEnumerator Cor_SpawnTimer()
     {
-        elapsedTime = 0.0f;
-        while (elapsedTime < secondsBetweenSpawn)
+        spawnDuration = 0.0f;
+        while (spawnDuration < finalSpawnTime)
         {
-            elapsedTime += Time.deltaTime;
+            spawnDuration += Time.deltaTime;
             yield return null;
         }
         yield return null;
@@ -97,21 +121,22 @@ public class BonusBat : MonoBehaviour
 
     #endregion
 
-   
     /// <summary>
     /// 코사인 이동경로로 이동한다.
     /// </summary>
-    /// <returns></returns>
     private IEnumerator MoveCosinePath()
     {
-        //float distance = Vector3.Distance(transform.position, targetPos.position);
-
         isFlying = true;
-        float runningTime = 0;
+        animator.SetBool("isFlying", isFlying);
+
+        //float distance = Vector3.Distance(transform.position, targetPos.position);
+        float time = 0;
+        totaldistance = 0;
+        distUnit = (targetPoint - startPoint).x / flightTime;
         while (isFlying/*Mathf.Abs(distance) >= 1*/)
         {
             //객체를 터치했다면 팝업창 열기
-            if(IsTouchedObject())
+            if (IsTouchedObject())
             {
                 if (onObjectTouched != null)
                 {
@@ -124,15 +149,35 @@ public class BonusBat : MonoBehaviour
             //distance = Vector3.Distance(transform.position, targetPos.position);
             //Debug.Log("distance" + distance);
             //transform.position = Vector3.Lerp(transform.position, targetPos.position, speed * Time.deltaTime);
-
-            runningTime += Time.deltaTime * speed;
-            float x = runningTime + startPos.x;
-            float y = Mathf.Cos(runningTime);
+            time += Time.deltaTime;
+            totaldistance = time * distUnit;
+            float x = startPoint.x + totaldistance;
+            float y = Mathf.Cos(totaldistance);
             transform.position = new Vector3(x, y, transform.position.z);
+
             yield return null;
         }
+    }
+    private Vector3[] cosinePoints;
 
-        OnReset();
+
+    /// <summary>
+    /// 코사인 경로의 지점들을 미리 계산한다.
+    /// </summary>
+    private void CalculateCosinePoints(int count)
+    {
+        cosinePoints = new Vector3[count+1];
+        float unit = Mathf.PI *2 / count;
+
+        float t = 0f;
+        float x, y;
+        for (int i = 0; i < count + 1; i++, t += unit)
+        {
+            x = t;
+            y = Mathf.Cos(t);
+            cosinePoints[i] = new Vector3(x, y, transform.position.z);
+        }
+
     }
 
     /// <summary>
@@ -141,7 +186,9 @@ public class BonusBat : MonoBehaviour
     private void EndTheFlight()
     {
         isFlying = false;
-        transform.position = startPos;
+        animator.SetBool("isFlying", isFlying);
+
+        transform.position = startPoint;
     }
 
 
@@ -151,18 +198,35 @@ public class BonusBat : MonoBehaviour
     /// <returns></returns>
     private bool IsTouchedObject()
     {
-        Vector2 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        RaycastHit2D hit = Physics2D.Raycast(touchPos, Camera.main.transform.forward);
-        if (hit.collider != null)
+        if (Input.GetMouseButtonDown(0))
         {
-            if (hit.collider.gameObject.tag.Equals("BonusBat"))
+            Vector3 touchPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            //충돌이 감지된 영역
+            RaycastHit2D hit = Physics2D.Raycast(touchPos,Camera.main.transform.forward);
+            //마우스 포인트 좌표를 만든다.
+            if (hit.collider !=null)
             {
-                return true;
+                Debug.Log("hit?" + hit.collider.gameObject.name);
+                if (hit.collider.tag == "BonusBat")
+                {
+                    return true;
+                }
             }
+
+            ////충돌이 감지된 영역
+            //RaycastHit hit;
+            ////마우스 포인트 좌표를 만든다.
+            //Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            //if (Physics.Raycast(ray.origin, ray.direction, out hit))
+            //{
+            //    Debug.Log("hit?" + hit.collider.gameObject.name);
+            //    if (hit.collider.tag == "BonusBat")
+            //    {
+            //        return true;
+            //    }
+            //}
         }
         return false;
     }
-
-  
    
 }
