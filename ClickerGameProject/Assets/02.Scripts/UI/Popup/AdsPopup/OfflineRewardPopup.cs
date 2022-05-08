df015a4b18ef17c5f480a64c0f83d2259322f75d
tpdms025 <seyang.dev@gmail.com>
2022-05-08 21:24:47 +0900

    Update Popup prefab

--- ClickerGameProject/Assets/02.Scripts/UI/Popup/AdsPopup/OfflineRewardPopup.cs
+++ ClickerGameProject/Assets/02.Scripts/UI/Popup/AdsPopup/OfflineRewardPopup.cs
@@ -8,22 +8,36 @@ using TMPro;
 
 public class OfflineRewardPopup : PopupWithAds
 {
-    private int maxTimeInMinute;
+    //최대 시간 (분)
+    private readonly int maxTimeInMinute = 240;    //4시간
+    //최소 시간 (분)
+    private readonly int minTimeInMinute = 10;    //10분
+    //현재 시간 (분)
     private int currentTimeInMinute;
-    private BigInteger baseRewardAmount;
-    private BigInteger addRewardAmount;
+
+    //기본 보상량
+    private double baseRewardAmount;
+    //추가 보상량
+    private double addRewardAmount;
 
     //기본 보상량에 곱할 배수
     private float basicMultiply;
     //추가 보상량에 곱할 배수
-    private float addMultiply = 3;
+    private float addMultiply = 3f;
 
+
+
+    #region Fields
 
     [SerializeField] private Image fill;
     [SerializeField] private TextMeshProUGUI maxTimeText;
     [SerializeField] private TextMeshProUGUI curTimeText;
     [SerializeField] private TextMeshProUGUI rewardText;
     [SerializeField] private TextMeshProUGUI addRewardText;
+
+    #endregion
+
+
 
 
     protected override void SubscribeToButtonEvents()
@@ -39,12 +53,18 @@ public class OfflineRewardPopup : PopupWithAds
     protected override void UnsubscribeFromButtonEvents()
     {
         base.UnsubscribeFromButtonEvents();
+        baseButton.onClick.RemoveAllListeners();
+        onAdsFinished -= ApplyAdditionalReward;
     }
 
     protected override void Start()
     {
         //맨 처음에만 초기화.
-        Initialize();
+        if (DBManager.Inst.firstStart == false)
+        {
+            Initialize();
+            DBManager.Inst.firstStart = true;
+        }
         base.Start();
     }
 
@@ -53,7 +73,7 @@ public class OfflineRewardPopup : PopupWithAds
     /// </summary>
     private void ApplyBasicReward()
     {
-        MoneyManager.Instance.AddJewel(baseRewardAmount);
+        MoneyManager.Inst.SumJewel(baseRewardAmount);
     }
 
     /// <summary>
@@ -61,7 +81,7 @@ public class OfflineRewardPopup : PopupWithAds
     /// </summary>
     private void ApplyAdditionalReward()
     {
-        MoneyManager.Instance.AddJewel(baseRewardAmount+addRewardAmount);
+        MoneyManager.Inst.SumJewel(addRewardAmount);
     }
 
     /// <summary>
@@ -69,23 +89,22 @@ public class OfflineRewardPopup : PopupWithAds
     /// </summary>
     private void Initialize()
     {
-        basicMultiply = 5.0f;
-        addMultiply = 3.0f;
-        maxTimeInMinute = 240; // 4시간
         currentTimeInMinute = OfflineTimeToMinute();
 
-        //오프라인 시간이 0이거나 보상이 0일 경우의 예외처리
-        if(currentTimeInMinute == 0 || MoneyManager.Instance.JewelPerTouch == 0)
+        basicMultiply = 5.0f;
+
+        //오프라인 시간이 최소시간보다 작거나 보상이 0일 경우의 예외처리
+        if(currentTimeInMinute < minTimeInMinute || MoneyManager.Inst.JewelPerTouch.Value == 0)
         {
 #if UNITY_EDITOR
             Debug.Log("시간을 받지 못하거나 혹은 보상이 0입니다.");
 #endif
-            ToggleOpenOrClose();
             return;
         }
         baseRewardAmount = GetRewardForTime(basicMultiply,currentTimeInMinute);
         addRewardAmount = GetAdditionalRewardForTime(addMultiply);
 
+        ToggleOpenOrClose();
         UpdateUI();
     }
 
@@ -93,9 +112,9 @@ public class OfflineRewardPopup : PopupWithAds
     /// 시간에 비례하여 기본보상을 가져온다.
     /// </summary>
     /// <returns></returns>
-    private BigInteger GetRewardForTime(float multiply, int _curTimeInMinute)
+    private double GetRewardForTime(float multiply, int _curTimeInMinute)
     {
-        BigInteger rewardData =  new BigInteger(5 * _curTimeInMinute) * MoneyManager.Instance.JewelPerTouch;
+        double rewardData =  (5 * _curTimeInMinute) * MoneyManager.Inst.JewelPerTouch.Value;
         return rewardData;
     }
 
@@ -103,9 +122,9 @@ public class OfflineRewardPopup : PopupWithAds
     /// 시간에 비례하여 추가보상을 가져온다. (기본보상의 N배)
     /// </summary>
     /// <returns></returns>
-    protected BigInteger GetAdditionalRewardForTime(float multiply)
+    protected double GetAdditionalRewardForTime(float multiply)
     {
-        BigInteger rewardData = baseRewardAmount * new BigInteger(addMultiply);
+        double rewardData = baseRewardAmount * (addMultiply);
         return rewardData;
     }
 
@@ -115,7 +134,7 @@ public class OfflineRewardPopup : PopupWithAds
     /// <returns></returns>
     private int OfflineTimeToMinute()
     {
-        TimeSpan offlineTime = TimerManager.Instance.offlineTimeSpan;
+        TimeSpan offlineTime = TimerManager.Inst.offlineTimeSpan;
 
         //최대시간 제한두기
         if (offlineTime.TotalMinutes > maxTimeInMinute)
@@ -144,6 +163,6 @@ public class OfflineRewardPopup : PopupWithAds
         fill.fillAmount = (float)currentTimeInMinute / maxTimeInMinute;
         maxTimeText.text = string.Format("최대 {0}시간", maxTimeInMinute/60);
         rewardText.text = string.Format("{0}", CurrencyParser.ToCurrencyString(baseRewardAmount));
-        addRewardText.text = string.Format("+{0} 추가 획득", CurrencyParser.ToCurrencyString(addRewardAmount));
+        addRewardText.text = string.Format("+{0} 획득", CurrencyParser.ToCurrencyString(addRewardAmount));
     }
 }
